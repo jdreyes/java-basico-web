4faac924d2993d3232b82f65856c5896ea549676
jdreyes <jreyes@autentia.com>
2015-11-17 16:59:53 +0100

    Init param en sample servlet

--- src/main/java/com/autentia/training/javabasico/web/servlet/SampleServlet.java
+++ src/main/java/com/autentia/training/javabasico/web/servlet/SampleServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.GenericServlet;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -17,14 +18,14 @@ public class SampleServlet extends GenericServlet{
 
 	
 	@Override
-	public void init() throws ServletException {
-		// TODO Auto-generated method stub
-		super.init();
+	public void init(ServletConfig config) throws ServletException {
+		
+		String sampleParam = config.getInitParameter("sampleParam");
+		System.out.println("Sample param:" + sampleParam);
 	}
 	
 	@Override
 	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		
 		//System.out.println("This is a sample servlet!");
 		
